60a9616cc727301aa96eee8920a7c70d0b61bdee
meelis utt <meelis.utt@gmail.com>
2023-09-09 14:16:47 +0200

    tweak reset_wifi script

--- bin/reset_wifi.sh
+++ bin/reset_wifi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xterm -e "doas kill -9 $(pgrep -x dhclient); /usr/sbin/dhclient -r; /usr/sbin/dhclient -4 -v -i -pf /run/dhclient.wlp2s0.pid -lf /var/lib/dhcp/dhclient.wlp2s0.leases -I -df /var/lib/dhcp/dhclient6.wlp2s0.leases wlp2s0"
+kill -9 $(pgrep -x dhclient); 
+su -c "/usr/sbin/dhclient -r; /usr/sbin/dhclient -4 -v -i -pf /run/dhclient.wlp2s0.pid -lf /var/lib/dhcp/dhclient.wlp2s0.leases -I -df /var/lib/dhcp/dhclient6.wlp2s0.leases wlp2s0"
