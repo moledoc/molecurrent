407419a5eb40fd715033560048c1abd18e25a1dc
meelis utt <meelis.utt@gmail.com>
2024-01-03 19:47:05 +0100

    update gh_bu script

--- bin/gh_bu.sh
+++ bin/gh_bu.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-set -xe
+set -x
 usr="$1"
 test -z "$usr" && echo "username expected" && exit 1
 mkdir -p $HOME/Documents/gh_bu
 cd $HOME/Documents/gh_bu
 curl --silent "https://api.github.com/users/$usr/repos" | grep html_url | sort | 9 sed 's/.*"html_url": "(.*)",/\1/g' | uniq -u | parallel git clone {}
+ls | parallel 'cd {} && git pull'
 cd -
